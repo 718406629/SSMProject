4a8ea66813782ff5a2c46f96e0c87d9b932cde37
zyq <718406629@qq.com>
2019-04-23 23:04:30 +0800

    第二次提交

--- SSMProject1/src/org/lanqiao/controller/StudentController.java
+++ SSMProject1/src/org/lanqiao/controller/StudentController.java
@@ -10,19 +10,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("controller")
-@Controller//StudentController加入容器
+@RequestMapping("controller")//处理请求映射
+@Controller//StudentController加入容器  2、使用Controller注解可以把普通类变成控制器
 public class StudentController {
     //控制器依赖与Service
-	@Autowired
-	@Qualifier("studentService") 
+	@Autowired//根据类型注入（会在IOC容器里寻找StudentService类型的bean）
+	@Qualifier("studentService") //根据名字注入（会在IOC容器里寻找id为studentService的bean）
     private StudentService studentService;
 	
 	public void setStudentService(StudentService studentService) {
 		this.studentService = studentService;
 	}
      
-	@RequestMapping("queryStudentByNo/{stuno}")
+	@RequestMapping("queryStudentByNo/{stuno}")//这里的map可以把student返回给request域 ，
 	public String queryStudentByNo(@PathVariable("stuno") Integer stuNo, Map<String,Object> map) {
 		Student student=studentService.queryStudentByStuno(stuNo);
 		map.put("student",student );
